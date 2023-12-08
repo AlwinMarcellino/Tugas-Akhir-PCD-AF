12505a74c39d2ec0c2b0d656cd72b584e5f58471
Alwin Marcellino <alwinmarch8323@gmail.com>
2023-12-08 17:49:55 +0700

    uodate

--- source/color_recognition_api/color_feature_extraction.py
+++ source/color_recognition_api/color_feature_extraction.py
@@ -13,7 +13,7 @@ from color_recognition_api import knn_classifier as knn_classifier
 def RGB2HEX(color):
     return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
 
-def color_extraction_of_test_image(test_src_image, number_of_colors, show_chart):
+def color_extraction_of_test_image(test_src_image, number_of_colors):
 
     # memuat gambar
     image = test_src_image
@@ -46,11 +46,6 @@ def color_extraction_of_test_image(test_src_image, number_of_colors, show_chart)
     with open('test.data', 'w') as myfile:
         for data in feature_data:
             myfile.write(data + '\n')
-
-    if show_chart:
-        plt.figure(figsize=(8, 6))
-        plt.pie(counts.values(), labels=feature_data, colors=hex_colors)
-        plt.show()
         
     total_pixels = sum(counts.values())
     percentages = []
@@ -60,9 +55,9 @@ def color_extraction_of_test_image(test_src_image, number_of_colors, show_chart)
         percentages.append(percentage)
         rgb_values.append(', '.join(str(int(c)) for c in rgb_color))
         print(f"Color {i + 1}: RGB = {', '.join(str(int(c)) for c in rgb_color)}, Percentage = {percentage:.2f}%")
-    return rgb_values, percentages
+    return hex_colors, rgb_values, percentages
 
-
+    
 def color_extraction_of_training_image(img_name):
 
     # mendeteksi warna gambar menggunakan nama file untuk melabel data training 
